432a26e7020729ee11c7698866781bc8b097aa17
lliioollcn <lliiooll@126.com>
2022-11-19 18:50:02 +0800

    1

---
+++ assets/AboutView.d809ad9d.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as t,c as o,a as s}from"./index.fddb6373.js";const _={},c={class:"about"},a=s("h1",null,"This is an about page",-1),n=[a];function r(i,u){return t(),o("div",c,n)}const f=e(_,[["render",r]]);export{f as default};
