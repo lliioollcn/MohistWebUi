3d78b4dd3476a87bf6f9f2218370b50365ed63bb
lliioollcn <lliiooll@126.com>
2022-11-19 21:12:23 +0800

    3

---
+++ assets/UserIndex.143fce33.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as t,a as _}from"./index.70a46de8.js";const o={},n={class:"about"},c=_("h1",null,"This is an about page",-1),a=[c];function r(d,i){return s(),t("div",n,a)}const u=e(o,[["render",r]]);export{u as default};
