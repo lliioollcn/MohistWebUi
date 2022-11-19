1d9186071475fda18db0400eea584840401156f3
lliioollcn <lliiooll@126.com>
2022-11-19 22:37:56 +0800

    4

---
+++ assets/UserIndex.f534ed20.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as t,a as _}from"./i18n.cd25a83c.js";const o={},n={class:"about"},c=_("h1",null,"This is an about page",-1),a=[c];function r(d,i){return s(),t("div",n,a)}const u=e(o,[["render",r]]);export{u as default};
